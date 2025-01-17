f3b74d80a4d46b42861c4f100302ca224342e522
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2025-01-17 22:10:18 +0530

    Refactor CartTotal title and improve Footer image alt attribute; enhance Orders and PlaceOrder pages with order display and payment method selection

--- frontend/src/components/CartTotal.jsx
+++ frontend/src/components/CartTotal.jsx
@@ -7,7 +7,7 @@ const CartTotal = () => {
   return (
     <div className='w-full'>
         <div className='text-2xl'>
-            <Title text1={'CART'} text2={'TOTAL'}/>
+            <Title text1={'CART '} text2={'TOTALS'}/>
 
         </div>
       <div className='flex flex-col gap-2 mt-2 text-sm'>
