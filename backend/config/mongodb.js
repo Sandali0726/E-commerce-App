19c537fb10dcecbdaa443956c62fc771e10b1625
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2025-01-18 12:00:17 +0530

    Implement user and admin login routes; update user controller and routes for better structure

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
+
 const connectDB = async () => {
-    mongoose.connection.on('connected',()=>{
-        console.log("DB Conected");
-        
-    })
-    
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
-}
+    mongoose.connection.on('connected', () => {
+        console.log("DB Connected");
+    });
+
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
+    } catch (error) {
+        console.error("DB Connection Error:", error);
+    }
+};
+
 export default connectDB;
