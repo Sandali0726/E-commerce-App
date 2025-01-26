146da6964dd39ddddcf83f7f2821efcb5d3dd059
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2025-01-26 23:50:38 +0530

    Add login functionality and enhance admin panel layout with Sidebar and improved Navbar

---
+++ admin/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react';
+
+const Login = () => {
+    const [email,setEmail] = useState('')
+    const [passsword,setPassword] = useState('')
+
+ const onSubmitHandler = async (e) =>{
+    try {
+        e.preventDefault();
+    } catch (error) {
+        
+    }
+ }
+
+  return (
+    <div className='min-h-screen flex items-center justify-center w-full'>
+      <div className='bg-white shadowmd rounded-lg px-8 py-6 max-w-md'>
+        <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
+        <form onSubmit={onSubmitHandler}>
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm font-medium text-gray-700mb-2 '>Email Address</p>
+                <input className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='your@email.com' required />
+            </div>
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm font-medium text-gray-700mb-2 '>Password</p>
+                <input className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="password" placeholder='Enter your password' required />
+            </div>
+            <button className = 'mt-2 w-full  py-2 px-4 rounded-md text-white bg-black' type='submit'>Login</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
